2aacb26e4addf85dcec9a1903f737bded07636b3
Will Kusper <wkusper@iu.edu>
2024-04-16 19:48:05 -0400

    azure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const { add } = require("./arithmetica");
 const app = express();
 app.use(cors());
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => { 
-    res.send('Arithmetic service - last updated 2/15/2024'); 
+    res.send('Arithmetic service - last updated 3/4/2024'); 
 });
 
 app.get('/add/:a/:b', (req, res) => { 
